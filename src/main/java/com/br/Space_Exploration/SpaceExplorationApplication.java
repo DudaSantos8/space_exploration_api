bba60c67e325360a69b580bb5ffe99f26b20332d
Duda <maria.oliveira@zup.com.br>
2024-12-06 11:06:27 -0300

    first commit

---
+++ src/main/java/com/br/Space_Exploration/SpaceExplorationApplication.java
@@ -0,0 +1,13 @@
+package com.br.Space_Exploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceExplorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceExplorationApplication.class, args);
+	}
+
+}
