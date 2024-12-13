a5c368fad991aa9aa247ec99dc0be75914a6aaf5
Sandra Mendes <sandra.mendes@gmail.com>
2024-12-13 10:28:02 -0300

    Add update spacecraft repository

--- src/main/java/com/br/Space_Exploration/App/ports/output/SpacecraftRepository.java
+++ src/main/java/com/br/Space_Exploration/App/ports/output/SpacecraftRepository.java
@@ -7,4 +7,5 @@ import com.br.Space_Exploration.infra.adapters.output.entities.SpacecraftEntity;
 public interface SpacecraftRepository {
     SpacecraftEntity getById(int id);
     SpacecraftEntity save (SpacecraftEntity registerDto);
+    SpacecraftEntity update(SpacecraftEntity spacecraft);
 }
