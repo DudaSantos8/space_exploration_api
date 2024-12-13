5b1eaa9c8f2846d4d888355503e0a086b2cd1736
Sandra Mendes <sandra.mendes@gmail.com>
2024-12-13 13:57:07 -0300

    Add random event on spacecraft repository

--- src/main/java/com/br/Space_Exploration/App/ports/output/SpacecraftRepository.java
+++ src/main/java/com/br/Space_Exploration/App/ports/output/SpacecraftRepository.java
@@ -1,11 +1,11 @@
 package com.br.Space_Exploration.App.ports.output;
 
+import com.br.Space_Exploration.Domain.dtos.EventResponseDto;
 import com.br.Space_Exploration.infra.adapters.output.entities.SpacecraftEntity;
 
 public interface SpacecraftRepository {
     SpacecraftEntity getById(int id);
-    SpacecraftEntity save (SpacecraftEntity registerDto);
     SpacecraftEntity update(SpacecraftEntity spacecraft);
     SpacecraftEntity save (SpacecraftEntity spacecraftEntity);
-
+    EventResponseDto generateRandomEvent();
 }
